476a27529ecb08df6d6fa0f3989049dd48be2ded
Evan McGullam <evan.mcgullam@stu.ocsb.ca>
2024-06-10 13:16:17 +0000

    started adding word guesser to replace underlines with letter, not working

--- Hangman.py
+++ Hangman.py
@@ -19,3 +19,21 @@ while guessloop == 0:
   guess = input("Beging guessing!").lower()
   found = False
 
+  for i in range (len(theword)):
+     if guess[i] == theword[i]: #need to fix index string error
+        board[i] = guess
+        found = True
+
+  if found == True:
+     print(" ".join(board))
+     if "_" not in board:
+        print("You guessed the word! You win!")
+        guessloop = 1
+     else:
+        lives = -1
+        print("Letter is not in word. You have", lives, "left.")
+        print(" ".join(board))
+
+if lives == 0:
+   print=("You ran out of lives. Valiant Effort!")
+   guessloop = 1
