bfca32485c44e7168da01d4efca121f9f4657f06
Evan McGullam <evan.mcgullam@stu.ocsb.ca>
2024-06-14 12:46:10 +0000

    fixed edge case for index.py

--- index.py
+++ index.py
@@ -1,18 +1,26 @@
-print("")
-print("Here are our games. To pick one, type the number of the game you would like to play!")
-selection = input("1. Blackjack, 2. Hangman, 3.Makeshift Dice, 4.TBD")
-if selection == "1":
-  from subprocess import call
-  def open_py_file():
-      call(["python", "Blackjack.py"])
-  open_py_file()
-elif selection == "2":
-  from subprocess import call
-  def open_py_file():
-      call(["python", "Hangman.py"])
-  open_py_file()
-elif selection == "3":
-  from subprocess import call
-  def open_py_file():
-      call(["python", "Poker.py"])
-  open_py_file()
+loop = 1
+
+while loop == 1:
+  print("")
+  print("Here are our games. To pick one, type the number of the game you would like to play!")
+  selection = input("1. Blackjack, 2. Hangman, 3.Makeshift Dice")
+  if selection == 1:
+    from subprocess import call
+    def open_py_file():
+          call(["python", "Blackjack.py"])
+    open_py_file()
+    loop = 0
+  elif selection == "2":
+      from subprocess import call
+      def open_py_file():
+          call(["python", "Hangman.py"])
+      open_py_file()
+      loop = 0
+  elif selection == "3":
+    from subprocess import call
+    def open_py_file():
+        call(["python", "Dice.py"])
+    open_py_file()
+    loop = 0
+  else:
+    print("Please enter a valid input.")
