b05ed4d349245cbbab88eece28664559c4ed7c2f
Evan McGullam <evan.mcgullam@stu.ocsb.ca>
2024-06-14 13:21:13 +0000

    fixed house bug

--- Dice.py
+++ Dice.py
@@ -2,6 +2,13 @@ import random
 playercash = 500
 dice1 = random.randint(1,6)
 dice2 = random.randint(1,6)
-print(dice1,dice2)
 
-#create serparte functions
+print("Welcome to Makeshift Dice!")
+print("In this game you roll dice, and either win or lose money! Simple. :)")
+print("")
+betamount = input("How much money would you like to bet this round?")
+playercash = (playercash)-(int(betamount))
+print("You bet",betamount,". Your new total is", playercash)
+print("")
+print("You can bet on, 1: Wether the dice are even or odd, 2: The Numbers of the Dice, 3: Higher or Lower than certain number")
+dicebet = input("Which option would you like(Type the number.).")
